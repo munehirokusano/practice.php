7f42eb2a992a9fd29aeec4405c35e3b0b0010009
名前 <>
2020-05-04 12:06:04 +0000

    課題提出

--- practice3.php
+++ practice3.php
@@ -52,58 +52,78 @@ echo multiplication_array(array(1 , 3 , 5 , 7 ,9)) . PHP_EOL;
 
 // 課題4
 
+// function max_array($arr4){
+    
+//     $max_number = max($arr4);
+    
+//     return $max_number;
+    
+// }
+
+// echo max_array(array(1 , 7 , 9 , 15)) . PHP_EOL;
+
 function max_array($arr4){
     
-    $max_number = max($arr4);
+    $max_number = $arr4[0];
     
-    return $max_number;
+    foreach ($arr4 as $a){
     
-}
-
-echo max_array(array(1 , 7 , 9 , 15)) . PHP_EOL;
-
-
-/// 課題5
-
-//strip_tag
-
-$text = "<h1>表紙</h1>";
-
-echo strip_tags($text) . PHP_EOL;
-
-
-//array push
-
-$fruits = array("apple" , "orange");
-
-array_push($fruits,"banana");
-
-print_r($fruits);
+        if ($max_number <= $a){
+        
+            $max_number = $a;
+        }
+    
+    }
+    
+    return $max_number;  
+        
+}        
+        
+    echo max_array(array(1 , 7 , 9 , 15)) . PHP_EOL;
 
 
-//array_merge
 
-$array1 = array(1 , 2, 3);
+// /// 課題5
 
-$array2 = array(5 , 7 , 9);
+// //strip_tag
 
-$total_array = array_merge($array1, $array2);
+// $text = "<h1>表紙</h1>";
 
-print_r($total_array);
-
-
-//time , mktime
-
-$time = time();
-
-echo $time . PHP_EOL;
+// echo strip_tags($text) . PHP_EOL;
 
 
-$time2 = mktime(11, 11, 11, 11, 11, 2011);
+// //array push
 
-var_dump($time2);
+// $fruits = array("apple" , "orange");
 
-echo date('Y年m月' , $time2);
+// array_push($fruits,"banana");
+
+// print_r($fruits);
+
+
+// //array_merge
+
+// $array1 = array(1 , 2, 3);
+
+// $array2 = array(5 , 7 , 9);
+
+// $total_array = array_merge($array1, $array2);
+
+// print_r($total_array);
+
+
+// //time , mktime
+
+// $time = time();
+
+// echo $time . PHP_EOL;
+
+
+// $time2 = mktime(11, 11, 11, 11, 11, 2011);
+
+// var_dump($time2);
+
+// echo date('Y年m月' , $time2);
 
 
 
